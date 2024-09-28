870db4cc37077ed6b53352c1ae215e32498c8046
Tomasz Węgrzyn <twgrzyn1@gmail.com>
2024-09-28 18:51:29 +0200

    init

---
+++ AsyncLocalDemo/Program.cs
@@ -0,0 +1,88 @@
+using AsyncLocalDemo;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using Serilog.Context;
+using Serilog.Events;
+using Serilog.Templates;
+using Serilog.Templates.Themes;
+using ILogger = Serilog.ILogger;
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console(LogEventLevel.Information)
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSerilog((services, lc) => lc
+    .ReadFrom.Configuration(builder.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(new ExpressionTemplate(
+        // Include trace and span ids when present.
+        "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} (LuckyNumber: {LuckyNumber}) (Trace: {@tr}, Span: {@sp}){#end}] {@m}\n{@x}",
+        theme: TemplateTheme.Code)));
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+
+builder.Services.AddSingleton<Service>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering",
+    "Scorching"
+};
+
+app.MapGet("/weatherforecast", ([FromServices] ILogger logger) =>
+    {
+        logger.Debug("Getting weather forecast");
+        LogContext.PushProperty("LuckyNumber", 69);
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        logger.Information("Got weather forecast");
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.MapGet("/DemoThreadLocal", ([FromServices] ILogger logger, [FromServices] Service service) =>
+    {
+        service.DemoThreadLocal();
+    })
+    .WithName("DemoThreadLocal")
+    .WithOpenApi();
+
+app.MapGet("/DemoThreadLocalAsync", async ([FromServices] ILogger logger, [FromServices] Service service) =>
+    {
+        await service.DemoThreadLocalAsync();
+    })
+    .WithName("DemoThreadLocalAsync")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ AsyncLocalDemo/Service.cs
@@ -0,0 +1,79 @@
+﻿using ILogger = Serilog.ILogger;
+
+namespace AsyncLocalDemo;
+
+public class Service
+{
+    private readonly ILogger _logger;
+    private readonly IHttpClientFactory _clientFactory;
+    private static ThreadLocal<int> _threadLocal = new();
+
+    public Service(ILogger logger, IHttpClientFactory clientFactory)
+    {
+        _logger = logger;
+        _clientFactory = clientFactory;
+    }
+
+    public void DemoThreadLocal()
+    {
+        // _threadLocal = new ThreadLocal<int>(() => Environment.CurrentManagedThreadId);
+        var threads = new Thread[3];
+
+        _threadLocal.Value = new Random().Next(1, 100);
+        _logger.Information("Thread ID: {CurrentThreadManagedThreadId}, Value: {ThreadLocalValue}", Environment.CurrentManagedThreadId, _threadLocal.Value);
+
+        for (var i = 0; i < threads.Length; i++)
+        {
+            threads[i] = new Thread(() =>
+            {
+                _threadLocal.Value = new Random().Next(1, 100);
+                var threadLocalValue = _threadLocal.Value;
+                _logger.Information("Spawned thread. Thread ID: {CurrentThreadManagedThreadId}, Value: {ThreadLocalValue}", Environment.CurrentManagedThreadId, threadLocalValue);
+            });
+
+            threads[i].Start();
+        }
+
+        foreach (var t in threads)
+        {
+            t.Join();
+        }
+        _logger.Information("Thread ID: {CurrentThreadManagedThreadId}, Value: {ThreadLocalValue}", Environment.CurrentManagedThreadId, _threadLocal.Value);
+
+        
+        _clientFactory.CreateClient(" :-) ").GetAsync("https://example.com").GetAwaiter().GetResult();
+        
+        _logger.Information("Thread ID: {CurrentThreadManagedThreadId}, Value: {ThreadLocalValue}", Environment.CurrentManagedThreadId, _threadLocal.Value);
+    }
+    
+    public async Task DemoThreadLocalAsync()
+    {
+        // _threadLocal = new ThreadLocal<int>(() => Environment.CurrentManagedThreadId);
+        var threads = new Thread[3];
+
+        _threadLocal.Value = new Random().Next(1, 100);
+        _logger.Information("Thread ID: {CurrentThreadManagedThreadId}, Value: {ThreadLocalValue}", Environment.CurrentManagedThreadId, _threadLocal.Value);
+
+        for (var i = 0; i < threads.Length; i++)
+        {
+            threads[i] = new Thread(() =>
+            {
+                _threadLocal.Value = new Random().Next(1, 100);
+                var threadLocalValue = _threadLocal.Value;
+                _logger.Information("Spawned thread. Thread ID: {CurrentThreadManagedThreadId}, Value: {ThreadLocalValue}", Environment.CurrentManagedThreadId, threadLocalValue);
+            });
+
+            threads[i].Start();
+        }
+
+        foreach (var t in threads)
+        {
+            t.Join();
+        }
+        _logger.Information("Thread ID: {CurrentThreadManagedThreadId}, Value: {ThreadLocalValue}", Environment.CurrentManagedThreadId, _threadLocal.Value);
+
+        await _clientFactory.CreateClient(" :-) ").GetAsync("https://example.com");
+        
+        _logger.Information("Thread ID: {CurrentThreadManagedThreadId}, Value: {ThreadLocalValue}", Environment.CurrentManagedThreadId, _threadLocal.Value);
+    }
+}
